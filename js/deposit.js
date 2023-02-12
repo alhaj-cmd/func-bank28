c1dd1ce9f41c7d283f4afa4d8af333534b92789c
github_username <you@domain.com>
2023-02-13 01:02:29 +0600

    functional file setting

--- js/deposit.js
+++ js/deposit.js
@@ -1,11 +1,19 @@
-function getInputFieldValueById(inputId){
-    const inputField = document.getElementById('deposite-field');
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseFloat(inputFieldValueString);
     return inputFieldValue;
 }
 
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText
+    const elementValue = parseFloat(elementValueString);
+    return elementValue;
+}
+
 
 document.getElementById('btn-deposite').addEventListener('click', function(){
-    console.log('hello')
+    // console.log('hello')
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 })
