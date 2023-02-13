a80ce4c79e802b15ec2bd724833a4ed51d865206
github_username <you@domain.com>
2023-02-13 19:06:32 +0600

    file setting

--- js/deposit.js
+++ js/deposit.js
@@ -1,21 +1,3 @@
-function getInputFieldValueById(inputFieldId){
-    const inputField = document.getElementById(inputFieldId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    return inputFieldValue;
-}
-
-function getElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText
-    const elementValue = parseFloat(elementValueString);
-    return elementValue;
-}
-
-function setTextElementValueById(elementId, newValue){
-    const textElement = document.getElementById(elementId);
-    textElement.innerText = newValue;
-}
 
 
 document.getElementById('btn-deposite').addEventListener('click', function(){
