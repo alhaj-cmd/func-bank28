00a9c5f4fed3bb568177a93961141e91ee8f9278
github_username <you@domain.com>
2023-02-13 19:04:56 +0600

    balance count

--- js/deposit.js
+++ js/deposit.js
@@ -26,4 +26,8 @@ document.getElementById('btn-deposite').addEventListener('click', function(){
 
     const newDepositTotal = previousDepositTotal + newDepositAmount;
     setTextElementValueById('deposit-total', newDepositTotal);
+    //  get previous balance by using the function
+    const previousBalanceTotal = getElementValueById('balance-total')
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
 })
