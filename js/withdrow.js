7d5de0f5cda249dfc05164bfe7ff08410ac422c3
github_username <you@domain.com>
2023-02-13 19:25:50 +0600

    withdrow added in balance

--- js/withdrow.js
+++ js/withdrow.js
@@ -1,1 +1,14 @@
-console.log('withdrow')
+// console.log('withdrow')
+document.getElementById('btn-withdrow').addEventListener('click', function(){
+    // console.log('hello')
+    const newWithdrowAmount = getInputFieldValueById('withdrow-field');
+    const previousWithdrowTotal = getElementValueById('withdrow-total');
+    // console.log(newDepositAmount,previousDepositTotal);
+
+    const newWithdrowTotal = previousWithdrowTotal + newWithdrowAmount;
+    setTextElementValueById('withdrow-total', newWithdrowTotal);
+    //  get previous balance by using the function
+    const previousBalanceTotal = getElementValueById('balance-total')
+    const newBalanceTotal = previousBalanceTotal - newWithdrowAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
